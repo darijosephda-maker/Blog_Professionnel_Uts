f3a82500e676c85d18960dd49af3ee5cd9801159
Dari Joseph Da <darijosephda@gmail.com>
2026-02-01 19:38:03 +0000

    Fix image display with whitenoise and urls

--- core/urls.py
+++ core/urls.py
@@ -1,28 +1,28 @@
 """
 URL configuration for core project.
 """
+
 from django.contrib import admin
-from django.urls import path, include # <-- Ajout de include
+from django.urls import path, include, re_path # On ajoute re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # Indispensable pour la production
 from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Au lieu de lier seulement 'home', on lie TOUTES les urls de l'app blog
     path('', include('blog.urls')), 
 ]
 
-# Servir les fichiers médias pendant le développement
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
-    
+# Cette partie s'occupe de servir les fichiers en TOUTES circonstances sur Render
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+
+# Ton script de création d'admin (à supprimer plus tard pour la sécurité)
 try:
     if not User.objects.filter(username='admin').exists():
-        # ICI : mets ton nom d'utilisateur, ton mail et ton mot de passe choisi
         User.objects.create_superuser('admin', 'darijosephda@gmail.com', 'Joseph2026!')
         print("Superutilisateur créé avec succès !")
 except Exception as e:
