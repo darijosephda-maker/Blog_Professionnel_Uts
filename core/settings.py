cd457a293624e3f46310dc34ee682c16cba4a946
Dari Joseph Da <darijosephda@gmail.com>
2026-02-01 18:04:22 +0000

    Mise à jour securite et hosts

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-=4ys_0##=%3&s4++t=^x)pm*oz@+zv1d9+!=4j1g1($%u4x)i4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['blog-professionnel-uts.onrender.com', '127.0.0.1', 'localhost', '*']# Application definition
-
+ALLOWED_HOSTS = ['blog-professionnel-uts.onrender.com', 'localhost', '127.0.0.1', '*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
