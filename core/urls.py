b3d1bfd9b26176ca1c2123516c5073b15e9b9fe0
Dari Joseph Da <darijosephda@gmail.com>
2026-02-01 18:39:23 +0000

    Creation du superuser admin

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include # <-- Ajout de include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,3 +18,12 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    
+try:
+    if not User.objects.filter(username='admin').exists():
+        # ICI : mets ton nom d'utilisateur, ton mail et ton mot de passe choisi
+        User.objects.create_superuser('admin', 'darijosephda@gmail.com', 'Joseph2026!')
+        print("Superutilisateur créé avec succès !")
+except Exception as e:
+    print(f"Erreur lors de la création de l'admin : {e}")
